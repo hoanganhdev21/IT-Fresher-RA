871e5314eeac6c577716d8ec680f0aad71bd319e
Your Name <you@example.com>
2023-11-27 16:14:28 +0700

    first commit

--- src/section2/Homework_Unit3.java
+++ src/section2/Homework_Unit3.java
@@ -1,11 +1,6 @@
 package section2;
 
 public class Homework_Unit3 {
-    public static void main(String[] args) {
-        System.out.println("Các số nguyên tố nhỏ hơn 100 là:");
-        hienThiSoNguyenTo();
-    }
-
     public static void hienThiSoNguyenTo() {
         for (int i = 2; i < 100; i++) {
             if (laSoNguyenTo(i)) {
---
+++ src/section3/Unit4.java
@@ -0,0 +1,19 @@
+package section3;
+
+import java.util.Scanner;
+
+public class Unit4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number;
+        System.out.println("Nhập số: ");
+        number = scanner.nextInt();
+        while (number != 0) {
+            System.out.println("Nhập số: ");
+            number = scanner.nextInt();
+            if (number % 3 == 0) {
+                System.out.println(number + " chia hết cho 3.");
+            }
+        }
+    }
+}
