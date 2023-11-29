127e9946addc348faaec235ef75d6e812b27a6ab
Your Name <you@example.com>
2023-11-29 16:39:46 +0700

    first commit

---
+++ src/ITFresher_NguyenHoangAnh/Exam01.java
@@ -0,0 +1,185 @@
+package ITFresher_NguyenHoangAnh;
+
+import java.util.Scanner;
+
+public class Exam01 {
+    public static void main(String[] args) {
+        int[] arr = new int[100];
+        int n = 0;
+        Scanner scanner = new Scanner(System.in);
+        int opt;
+        do {
+            System.out.println("\n!!<<<<<<<<<<<<<<<<<<<<< MENU >>>>>>>>>>>>>>>>>>>>>!!\n");
+            System.out.println("1. Nhập giá trị n phần tử của mảng (n nhập từ bàn phím)");
+            System.out.println("2. In giá trị các phần tử trong mảng");
+            System.out.println("3. Tính trung bình các phần tử dương (>0) trong mảng");
+            System.out.println("4. In ra vị trí (chỉ số) các phần tử có giá trị bằng k trong mảng (k nhập từ bàn phím)");
+            System.out.println("5. Sử dụng thuật toán sắp xếp nổi bọt sắp xếp mảng giảm dần");
+            System.out.println("6. Tính số lượng các phần tử là số nguyên tố trong mảng");
+            System.out.println("7. Sắp xếp các phần tử chẵn chia hết cho 3 ở đầu mảng theo thứ tự tăng dần, các phần tử lẻ chia hết cho 3 ở cuối mảng theo thứ tự giảm dần, các phần tử còn lại ở giữa mảng theo thứ tự tăng dần");
+            System.out.println("8. Nhập giá trị m từ bàn phím, chèn giá trị m vào mảng (sắp xếp giảm dần) đúng vị trí");
+            System.out.println("9. Thoát");
+            System.out.println("Vui lòng chọn chức năng từ 1 >>> 9 xin cảm ơn!:");
+            opt = scanner.nextInt();
+            switch (opt) {
+                case 1:
+                    // Nhập giá trị n phần tử của mảng
+                    System.out.println("Nhập số lượng phần tử của mảng:");
+                    n = scanner.nextInt();
+                    System.out.println("Nhập giá trị cho từng phần tử của mảng:");
+                    for (int i = 0; i < n; i++) {
+                        System.out.println("Nhập giá trị cho phần tử thứ " + (i + 1) + ":");
+                        arr[i] = scanner.nextInt();
+                    }
+                    break;
+                case 2:
+                    // In giá trị các phần tử trong mảng
+                    System.out.println("Giá trị của các phần tử trong mảng:");
+                    for (int i = 0; i < n; i++) {
+                        System.out.println("Phần tử thứ " + (i + 1) + ": " + arr[i]);
+                    }
+                    break;
+                case 3:
+                    // Tính trung bình các phần tử dương (>0) trong mảng
+                    int sum = 0;
+                    int count = 0;
+                    for (int i = 0; i < n; i++) {
+                        if (arr[i] > 0) {
+                            sum += arr[i];
+                            count++;
+                        }
+                    }
+                    if (count > 0) {
+                        double average = (double) sum / count;
+                        System.out.println("Trung bình các phần tử dương trong mảng: " + average);
+                    } else {
+                        System.out.println("Không có phần tử dương nào trong mảng.");
+                    }
+                    break;
+                case 4:
+                    // In ra vị trí (chỉ số) các phần tử có giá trị bằng k trong mảng
+                    System.out.print("Nhập giá trị k cần tìm: ");
+                    int k = scanner.nextInt();
+
+                    System.out.print("Vị trí (chỉ số) của phần tử có giá trị " + k + " trong mảng: ");
+                    boolean found = false;
+                    for (int i = 0; i < n; i++) {
+                        if (arr[i] == k) {
+                            System.out.print(i + " ");
+                            found = true;
+                        }
+                    }
+                    if (!found) {
+                        System.out.println("Không tìm thấy giá trị " + k + " trong mảng.");
+                    } else {
+                        System.out.println();
+                    }
+                    break;
+                case 5:
+                    // Sử dụng thuật toán sắp xếp nổi bọt sắp xếp mảng giảm dần
+                    for (int i = 0; i < n; i++) {
+                        for (int j = 0; j < n - i - 1; j++) {
+                            if (arr[j] < arr[j + 1]) {
+                                int temp = arr[j];
+                                arr[j] = arr[j + 1];
+                                arr[j + 1] = temp;
+                            }
+                        }
+                    }
+                    System.out.print("Mảng sau khi sắp xếp giảm dần: ");
+                    for (int i = 0; i < n; i++) {
+                        System.out.print(arr[i] + " ");
+                    }
+                    break;
+                case 6:
+                    // Tính số lượng các phần tử là số nguyên tố trong mảng
+                    int count2 = 0;
+                    for (int i = 0; i < n; i++) {
+                        boolean isPrime = true;
+
+                        if (arr[i] < 2) {
+                            isPrime = false;
+                        } else {
+                            for (int j = 2; j <= Math.sqrt(arr[i]); j++) {
+                                if (arr[i] % j == 0) {
+                                    isPrime = false;
+                                    break;
+                                }
+                            }
+                        }
+                        if (isPrime) {
+                            count2++;
+                        }
+                    }
+                    System.out.println("Số lượng phần tử là số nguyên tố trong mảng là: " + count2);
+                    break;
+                case 7:
+                    // Sắp xếp các phần tử chẵn chia hết cho 3 ở đầu mảng theo thứ tự tăng dần,
+                    // các phần tử lẻ chia hết cho 3 ở cuối mảng theo thứ tự giảm dần, các phần tử
+                    // còn lại ở giữa mảng theo thứ tự tăng dần
+                    // Sắp xếp mảng theo yêu cầu
+                    for (int i = 0; i < n - 1; i++) {
+                        for (int j = i + 1; j < n; j++) {
+                            // Sắp xếp phần tử chẵn chia hết cho 3
+                            if (arr[i] % 2 == 0 && arr[i] % 3 == 0 && arr[j] % 2 == 0 && arr[j] % 3 == 0) {
+                                if (arr[i] > arr[j]) {
+                                    int temp = arr[i];
+                                    arr[i] = arr[j];
+                                    arr[j] = temp;
+                                }
+                            }
+                            // Sắp xếp phần tử lẻ chia hết cho 3
+                            else if (arr[i] % 2 != 0 && arr[i] % 3 == 0 && arr[j] % 2 != 0 && arr[j] % 3 == 0) {
+                                if (arr[i] < arr[j]) {
+                                    int temp = arr[i];
+                                    arr[i] = arr[j];
+                                    arr[j] = temp;
+                                }
+                            }
+                            // Sắp xếp phần tử còn lại
+                            else if (arr[i] > arr[j]) {
+                                int temp = arr[i];
+                                arr[i] = arr[j];
+                                arr[j] = temp;
+                            }
+                        }
+                    }
+
+                    // Hiển thị mảng sau khi sắp xếp
+                    System.out.println("Mảng sau khi sắp xếp:");
+                    for (int i = 0; i < n; i++) {
+                        System.out.print(arr[i] + " ");
+                    }
+                    break;
+                case 8:
+                    // Nhập giá trị m từ bàn phím, chèn giá trị m vào mảng (sắp xếp giảm dần) đúng vị trí
+                    System.out.print("Nhập giá trị m: ");
+                    int m = scanner.nextInt();
+                    int[] newArray = new int[n + 1];
+                    int i;
+                    for (i = 0; i < n; i++) {
+                        if (arr[i] < m) {
+                            newArray[i] = arr[i];
+                        } else {
+                            break;
+                        }
+                    }
+                    newArray[i] = m;
+                    for (int j = i; j < n; j++) {
+                        newArray[j + 1] = arr[j];
+                    }
+                    System.out.print("Mảng sau khi chèn: ");
+                    for (int value : newArray) {
+                        System.out.print(value + " ");
+                    }
+                    break;
+                case 9:
+                    System.out.println("Thoát chương trình.");
+                    break;
+                default:
+                    System.out.println("Lựa chọn không hợp lệ. Vui lòng thử lại.");
+            }
+        } while (opt != 9);
+    }
+}
+
---
+++ src/ITFresher_NguyenHoangAnh/Exam2.java
@@ -0,0 +1,210 @@
+package ITFresher_NguyenHoangAnh;
+
+import java.util.Scanner;
+
+public class Exam2 {
+    public static void main(String[] args) {
+        // Tạo đối tượng Scanner
+        Scanner scanner = new Scanner(System.in);
+//        int[][] matrix;
+        int rows, cols;
+
+        // Nhập số dòng và số cột của mảng
+        System.out.print("Nhập số dòng của ma trận: ");
+        rows = scanner.nextInt();
+        System.out.print("Nhập số cột của ma trận: ");
+        cols = scanner.nextInt();
+
+        // Khai báo và khởi tạo mảng 2 chiều
+        int[][] arr = new int[rows][cols];
+        int opt;
+
+        do {
+            System.out.println("************************MENU**************************");
+            System.out.println("1. Nhập giá trị các phần tử của mảng");
+            System.out.println("2. In giá trị các phần tử trong mảng theo ma trận");
+            System.out.println("3. Tính số lượng các phần tử chia hết cho 2 và 3 trong mảng");
+            System.out.println("4. In các phần tử và tổng các phần tử nằm trên đường biên, đường chéo chính và đường chéo phụ");
+            System.out.println("5. Sử dụng thuật toán sắp xếp lựa chọn sắp xếp các phần tử tăng dần theo cột của mảng");
+            System.out.println("6. In ra các phần tử là số nguyên tố trong mảng");
+            System.out.println("7. Sử dụng thuật toán chèn (Insertion sort) sắp xếp các phần tử trên đường chéo chính của mảng giảm dần");
+            System.out.println("8. Nhập giá trị một mảng 1 chiều gồm m phần tử và chỉ số dòng muốn chèn vào mảng");
+            System.out.println("9. Thoát");
+
+            System.out.print("Nhập lựa chọn của bạn: ");
+            opt = scanner.nextInt();
+
+            switch (opt) {
+                case 1:
+                    // Nhập giá trị cho các phần tử trong mảng
+                    System.out.println("Nhập giá trị cho các phần tử của mảng:");
+                    for (int i = 0; i < arr.length; i++) {
+                        for (int j = 0; j < arr[i].length; j++) {
+                            System.out.print("Nhập giá trị cho phần tử [" + i + "][" + j + "]: ");
+                            arr[i][j] = scanner.nextInt();
+                        }
+                    }
+                    break;
+                case 2:
+                    // In giá trị các phần tử trong mảng theo ma trận
+                    System.out.println("Giá trị các phần tử mảng theo ma trận:");
+                    for (int i = 0; i < arr.length; i++) {
+                        for (int j = 0; j < arr[i].length; j++) {
+                            System.out.print(arr[i][j] + "\t");
+                        }
+                        System.out.println();
+                    }
+                    break;
+                case 3:
+                    // Tính số lượng các phần tử chia hết cho 2 và 3 trong mảng
+                    int count = 0;
+                    for (int i = 0; i < rows; i++) {
+                        for (int j = 0; j < cols; j++) {
+                            if (arr[i][j] % 2 == 0 && arr[i][j] % 3 == 0) {
+                                count++;
+                            }
+                        }
+                    }
+                    System.out.println("Số lượng phần tử chia hết cho 2 và 3 trong mảng là: " + count);
+                    break;
+                case 4:
+                    // In các phần tử và tổng các phần tử nằm trên đường biên, đường chéo chính và đường chéo phụ
+                    int sum = 0;
+                    int n = arr.length;
+                    for (int i = 0; i < n; i++) {
+                        for (int j = 0; j < n; j++) {
+                            // Kiểm tra nếu phần tử nằm trên đường biên, đường chéo chính hoặc đường chéo phụ
+                            if (i == 0 || i == n - 1 || j == 0 || j == n - 1 || i == j || i == n - j - 1) {
+                                System.out.println(arr[i][j]);
+                                sum += arr[i][j];
+                            }
+                        }
+                    }
+                    System.out.println("Tổng các phần tử nằm trên đường biên, đường chéo chính và đường chéo phụ: " + sum);
+
+                    break;
+                case 5:
+                    // Sử dụng thuật toán sắp xếp lựa chọn sắp xếp các phần tử tăng dần theo cột của mảng
+                    for (int j = 0; j < cols; j++) {
+                        for (int i = 0; i < rows - 1; i++) {
+                            int minIndex = i;
+                            for (int k = i + 1; k < rows; k++) {
+                                if (arr[k][j] < arr[minIndex][j]) {
+                                    minIndex = k;
+                                }
+                            }
+                            if (minIndex != i) {
+                                int temp = arr[i][j];
+                                arr[i][j] = arr[minIndex][j];
+                                arr[minIndex][j] = temp;
+                            }
+                        }
+                    }
+                    // In ra ma trận sau khi đã sắp xếp
+                    for (int i = 0; i < cols; i++) {
+                        for (int j = 0; j < rows; j++) {
+                            System.out.print(arr[i][j] + " ");
+                        }
+                        System.out.println();
+                    }
+                    break;
+                case 6:
+                    System.out.println("Các phần tử là số nguyên tố trong mảng:");
+                    for (int i = 0; i < rows; i++) {
+                        for (int j = 0; j < cols; j++) {
+                            int currentNumber = arr[i][j];
+                            boolean isPrime = true;
+
+                            if (currentNumber < 2) {
+                                isPrime = false;
+                            } else {
+                                for (int k = 2; k <= Math.sqrt(currentNumber); k++) {
+                                    if (currentNumber % k == 0) {
+                                        isPrime = false;
+                                        break;
+                                    }
+                                }
+                            }
+                            if (isPrime) {
+                                System.out.print(currentNumber + " ");
+                            }
+                        }
+                    }
+                    System.out.println(); // Xuống dòng sau khi kết thúc
+
+                    break;
+                case 7:
+                    // Nhập kích thước của ma trận vuông
+                    System.out.print("Nhập kích thước của ma trận vuông: ");
+                    int size = scanner.nextInt();
+
+                    int[][] maTrix = new int[size][size];
+
+                    // Nhập giá trị cho ma trận
+                    System.out.println("Nhập giá trị cho ma trận:");
+                    for (int i = 0; i < size; i++) {
+                        for (int j = 0; j < size; j++) {
+                            System.out.print("Nhập giá trị cho phần tử [" + i + "][" + j + "]: ");
+                            maTrix[i][j] = scanner.nextInt();
+                        }
+                    }
+
+                    // Sắp xếp các phần tử trên đường chéo chính giảm dần
+                    for (int i = 1; i < size; i++) {
+                        int giaTriHienTai = maTrix[i][i];
+                        int j = i - 1;
+
+                        while (j >= 0 && maTrix[j][j] < giaTriHienTai) {
+                            maTrix[j + 1][j + 1] = maTrix[j][j];
+                            j--;
+                        }
+
+                        maTrix[j + 1][j + 1] = giaTriHienTai;
+                    }
+
+                    // Hiển thị ma trận sau khi sắp xếp
+                    System.out.println("Ma trận sau khi sắp xếp các phần tử trên đường chéo chính giảm dần:");
+                    for (int i = 0; i < size; i++) {
+                        for (int j = 0; j < size; j++) {
+                            System.out.print(maTrix[i][j] + " ");
+                        }
+                        System.out.println();
+                    }
+                    break;
+                case 8:
+                    System.out.println("Nhập số phần tử của mảng:");
+                    int m = scanner.nextInt();
+                    int[] arrs = new int[m];
+                    System.out.println("Nhập các phần tử của mảng:");
+                    for(int i = 0; i < m; i++){
+                        arrs[i] = scanner.nextInt();
+                    }
+                    System.out.println("Nhập chỉ số dòng muốn chèn:");
+                    int index = scanner.nextInt();
+                    int[][] matrix = new int[3][3];
+                    System.out.println("Nhập các phần tử của ma trận:");
+                    for(int i = 0; i < 3; i++){
+                        for(int j = 0; j < 3; j++){
+                            matrix[i][j] = scanner.nextInt();
+                        }
+                    }
+                    // Chèn mảng vào ma trận tại chỉ số dòng đã cho
+                    matrix[index] = arrs;
+                    System.out.println("Ma trận sau khi chèn:");
+                    for(int i = 0; i < 3; i++){
+                        for(int j = 0; j < 3; j++){
+                            System.out.print(matrix[i][j] + " ");
+                        }
+                        System.out.println();
+                    }
+                    break;
+                case 9:
+                    System.out.println("Chương trình kết thúc.");
+                    break;
+                default:
+                    System.out.println("Lựa chọn không hợp lệ. Vui lòng chọn lại.");
+            }
+        } while (opt != 9);
+    }
+}
+
