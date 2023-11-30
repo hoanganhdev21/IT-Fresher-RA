3f12302254dff723e78c5a6ee14f73d0ce4601d9
Your Name <you@example.com>
2023-12-01 02:51:15 +0700

    first commit

---
+++ src/Section2_If_ElseIf_Else/switch_case.java
@@ -0,0 +1,8 @@
+package Section2_If_ElseIf_Else;
+
+public class switch_case {
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ src/Section4_Arrays/ArraysDemo.java
@@ -0,0 +1,8 @@
+package Section4_Arrays;
+
+public class ArraysDemo {
+    // 1. Khai baáo mảng number khai báo mảng 1 chiều số nguyên gồm 5 số nguyên number gồm 5 phần tử
+    // Datatype arrayName = new datatype[size]
+    int[] numbers = new int[5];
+
+}
