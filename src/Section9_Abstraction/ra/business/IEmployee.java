b5a5d39b76c3d22b03941c5df151f1480e04d908
Your Name <you@example.com>
2023-12-06 17:22:15 +0700

    first commit

---
+++ src/Section9_Abstraction/ra/business/IEmployee.java
@@ -0,0 +1,11 @@
+package Section9_Abstraction.ra.business;
+
+import java.util.Scanner;
+
+public interface IEmployee {
+    float BASIC_SALARY = 1300000;
+
+    void inputData(Scanner scanner);
+
+    void displayData();
+}
